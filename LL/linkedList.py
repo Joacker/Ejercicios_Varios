d636d635849ba7dcb12154088762058254dc4116
Joacker <jfernandezvizcarra@gmail.com>
2024-09-12 00:10:49 -0300

    new commit

--- LL/linkedList.py
+++ LL/linkedList.py
@@ -28,6 +28,23 @@ class LinkedList:
     def insert(self, index, value):
         pass
     
+    def pop(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.head
+            pre = self.head
+            while temp.next:
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            pre.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return temp.value
+    
     def print_list(self):
         temp = self.head
         while temp is not None:
