637f9d56c55105f1c7f524aa4cffb3b74fe725d4
Joacker <jfernandezvizcarra@gmail.com>
2024-09-11 23:55:59 -0300

    new commit

--- LL/linkedList.py
+++ LL/linkedList.py
@@ -11,14 +11,36 @@ class LinkedList:
         self.length = 1
     
     def append(self, value):
-        pass
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            temp = self.tail
+            temp.next = new_node
+            self.tail = new_node
+            self.length += 1
+        return True
         
     def prepend(self, value):
         pass
         
     def insert(self, index, value):
         pass
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
 my_linked_list = LinkedList(4)
 
-print(my_linked_list.head.value) # 4
+#print(my_linked_list.head.value) # 4
+
+#my_linked_list.print_list() # 4
+
+my_linked_list.append(3)
+my_linked_list.append(4)
+
+my_linked_list.print_list() # 4, 3
